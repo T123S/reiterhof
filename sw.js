9bb9233799d5eb11b861c7987f1e13224115a9a4
T123S <98583520+T123S@users.noreply.github.com>
2026-01-19 23:13:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pferdehof-cache-v1';
+const CACHE_NAME = 'pferdehof-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -42,4 +42,5 @@ self.addEventListener('activate', event => {
       );
     })
   );
+
 });
